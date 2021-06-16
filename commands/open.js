a7c0296b8569fb541556b26a04cad6b6665f8c33
SuperBOY000 <71724550+SuperBOY000@users.noreply.github.com>
2021-06-16 19:00:49 +0200

    .

--- commands/open.js
+++ commands/open.js
@@ -9,11 +9,11 @@ module.exports = {
 	ownerOnly: false,
 	async execute(client, message, args, prefix) {
 
-        var winkelName = args.splice(0, args.length).join(" ").toLowerCase()
+        var winkelName = args.splice(0, args.length).join(" ")
 
         const result = await schema.findOne({ serverId: message.guild.id, name: winkelName })
         if (!result) return message.reply("Winkel niet gevonden!")
-        
+
 		await schema.findOneAndUpdate(
             {
                 serverId: message.guild.id,
