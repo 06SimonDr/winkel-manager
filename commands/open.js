0f8d6d32bac4fbae62fa70e14adedd47aa6da08b
SuperBOY000 <71724550+SuperBOY000@users.noreply.github.com>
2021-06-17 18:52:28 +0200

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -15,7 +15,7 @@ module.exports = {
         if (!result) return message.reply("Winkel niet gevonden!")
         if (!result.medewerkers.includes(message.author.id)) {
             result.roles.forEach(async role => {
-                if(message.author.roles.cache.has(role)) {
+                if(message.member.roles.cache.has(role)) {
                     await schema.findOneAndUpdate(
                         {
                             serverId: message.guild.id,
