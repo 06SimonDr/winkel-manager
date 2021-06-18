c100f6e29480069e9934cf52f6e9ac6163d9da6c
SuperBOY000 <71724550+SuperBOY000@users.noreply.github.com>
2021-06-18 11:39:01 +0200

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -15,26 +15,33 @@ module.exports = {
         const result = await schema.findOne({ serverId: message.guild.id, name: winkelName })
         const result2 = await medwSchema.findOne({ serverId: message.guild.id, userId: message.author.id })
         if (result2) if (result2.activeWinkel !== null) return message.reply(`Meld je eerst af bij ${result2.activeWinkel}, voor je een nieuwe winkel opent!`)
+        if (!result2.winkels.includes(winkelName)) {
         if (!result) return message.reply("Winkel niet gevonden!")
             result.roles.forEach(async role => {
                 if(message.member.roles.cache.has(role)) {
+                    var perms = true
                     await medwSchema.findOneAndUpdate(
                         { serverId: message.guild.id, userId: message.author.id },
                         { serverId: message.guild.id, userId: message.author.id, activeWinkel: winkelName, $push: { winkels: winkelName } },
                         { upsert: true }
                     )
-
-                    await schema.findOneAndUpdate(
-                        { serverId: message.guild.id, name: winkelName },
-                        { serverId: message.guild.id, name: winkelName, $inc: { active: 1 } },
-                        { upsert: true }
-                    )
-            
-                    var embed = new discord.MessageEmbed()
-                        .setTitle(`Je bent nu aanwezig bij ${winkelName}!`)
-            
-                    message.channel.send(embed)
                 }
             })
+        } else var perms = true
+
+        if (perms === true) {
+            await schema.findOneAndUpdate(
+                { serverId: message.guild.id, name: winkelName },
+                { serverId: message.guild.id, name: winkelName, $inc: { active: 1 } },
+                { upsert: true }
+            )
+    
+            var embed = new discord.MessageEmbed()
+                .setTitle(`Je bent nu aanwezig bij ${winkelName}!`)
+    
+            message.channel.send(embed)
+        } else {
+            return message.reply("Je werkt niet bij deze winkel!")
+        }
 	},
 };
